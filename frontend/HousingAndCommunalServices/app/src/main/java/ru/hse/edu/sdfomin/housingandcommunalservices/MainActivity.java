01e5fed993476ffd71a9f59b6600816cedd8a031
Сергей Фомин <sdfomin@edu.hse.ru>
2020-05-16 16:04:40 +0300

    another one empty android project

---
+++ frontend/HousingAndCommunalServices/app/src/main/java/ru/hse/edu/sdfomin/housingandcommunalservices/MainActivity.java
@@ -0,0 +1,14 @@
+package ru.hse.edu.sdfomin.housingandcommunalservices;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
